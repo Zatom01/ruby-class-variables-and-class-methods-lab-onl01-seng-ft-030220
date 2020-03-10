d142b2fa255968e84b28579f8fd7ac561eab3f67
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-10 02:26:08 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,12 +3,14 @@ class song
   
   @@count=0 
   @@genres=[]
+  @@artists=[]
   
   def initialize(name,artist,genre)
     @name,@artist,@genre
     
     @@count+=1 
     @@genres<<genre 
+    @@artists<<artist 
   end
   
   def self.count
@@ -16,8 +18,12 @@ class song
   end 
   
   def self.genres
-    @@genres.u
+    @@genres.uniq
     
+  end 
+  
+  def self.artists
+  
   end 
 
 
