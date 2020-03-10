e2522c838572de8b1d09e9c6823524c18ee33b9f
Anil Gurung <anil_gurung15@yahoo.com>
2020-03-10 02:59:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,7 +4,6 @@ class Song
   @@count=0 
   @@genres=[]
   @@artists=[]
-  @@artist_count=[]
   
   def initialize(name,artist,genre)
     @name=name
@@ -44,6 +43,17 @@ class Song
     
     genre_count
   end
+  
+  def self.artist_count
+  artist_count={}
+  @@artists.each do |artist|
+    if artist_count[artist]
+      artist_count[artist]+=1 
+    else
+      artist_count[artist]=1 
+  
+  end 
+  
 
 
 end
